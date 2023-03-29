5d2d5c231dfbfe750a1e03f3e55c97fd182ab821
musabtrumboo <musabtrumboo@gmail.com>
2023-03-30 01:24:43 +0530

    Simplified Startup.cs file by creating Extension methods for IServiceCollection

--- src/Play.Catalog.Service/Startup.cs
+++ src/Play.Catalog.Service/Startup.cs
@@ -35,25 +35,11 @@ namespace Play.Catalog.Service
         public void ConfigureServices(IServiceCollection services)
         {
 
-            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
-            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-
             serviceSettings = Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
 
-            services.AddSingleton(serviceProvider =>
-            {
-
-                var mongoDbSettings = Configuration.GetSection(nameof(MongoDBSettings)).Get<MongoDBSettings>();
-                var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
-                return mongoClient.GetDatabase(serviceSettings.ServiceName);
-            });
-
-            services.AddSingleton<IRepository<Item>>(serviceProvider => 
-            {
-                var database = serviceProvider.GetService<IMongoDatabase>();
-                return new MongoRepository<Item>(database,"items");
-            });
-
+            services.AddMongo()
+                .AddMongoRepository<Item>("items");
+            
             services.AddControllers(options =>
             {
                 options.SuppressAsyncSuffixInActionNames = false; // dotnet runtime removes Async suffixes from the method names
